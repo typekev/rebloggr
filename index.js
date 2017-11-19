f589cba3b0b7e974fbfbede309af277f6968b508
typekev <kev@typekev.com>
2017-11-19 01:39:38 +0100

    link is now passed through the JSON post, rather than  Rebloggr.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const genPosts = (blog, props) => blog.posts.map((post, index) => {
-    const { titleIcon, cta, link } = props
-    const { title, date, content } = post
+    const { titleIcon, cta } = props
+    const { title, date, content, link } = post
     return (<div key={index}>
         <a className="reblog-link" href={link}><h2 className="reblog-title">{titleIcon}{title}</h2>
             <h6 className="reblog-date">{date}</h6>
