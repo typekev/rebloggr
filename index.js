f9624c014869a1277e0fe045bfef1e91df5abcd6
typekev <kev@typekev.com>
2017-11-19 01:26:07 +0100

    now include a link - use rebloggr to link to main blog article.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const genPosts = (blog, props) => blog.posts.map((post, index) => {
-    const { titleIcon, cta } = props
+    const { titleIcon, cta, link } = props
     const { title, date, content } = post
     return (<div key={index}>
-        <h2 className="rebloggr-title">{titleIcon}{title}</h2>
-        <h6 className="rebloggr-date">{date}</h6>
-        <h3 className="rebloggr-content">{content}</h3>
-        {props.cta ? <div className="rebloggr-cta">{cta}</div> : null}
+        <a className="reblog-link" href={link}><h2 className="reblog-title">{titleIcon}{title}</h2>
+            <h6 className="reblog-date">{date}</h6>
+            <h3 className="reblog-content">{content}</h3></a>
+        {props.cta ? <div className="reblog-cta">{cta}</div> : null}
     </div>)
 });
 
